eea89b7c555ba0873791e4c152e0f9d9107d69ea
noteconvert <me@ashley.how>
2019-12-15 19:17:49 +0100

    first commit

---
+++ precache-manifest.9bb941476c0f8f21feaef7167958adb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b5835ee3fe5c155c6e3d18290501de",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "492e44462ff8e16161ab",
+    "url": "/meme-generator/static/css/main.c7bd73d2.chunk.css"
+  },
+  {
+    "revision": "e2fa0279e3b395d83e8f",
+    "url": "/meme-generator/static/js/2.6d15567d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/meme-generator/static/js/2.6d15567d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "492e44462ff8e16161ab",
+    "url": "/meme-generator/static/js/main.917e4dc0.chunk.js"
+  },
+  {
+    "revision": "490a007f6bdc41ad7f8d",
+    "url": "/meme-generator/static/js/runtime-main.823f600b.js"
+  }
+]);
